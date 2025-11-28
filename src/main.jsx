488895c53387150256715692e771eae0224942f4
mrjemmy <jbpatel1411@gmail.com>
2025-11-28 18:00:03 +0530

    last test jay mataju

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import UserProvider from "./context/userProvider";
+import App from "./App.jsx";
+import UserProvider from "./context/userProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
