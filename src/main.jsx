b89c68c6a0461409c1afc5076d4970fb88c51a62
mrjemmy <jbpatel1411@gmail.com>
2025-11-28 17:44:22 +0530

    re upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import UserProvider from "./context/userProvider.jsx";
+import App from "./App";
+import UserProvider from "./context/userProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
